1e3e9a74930dc26e14b9e90a147e8af531763a0e
Victor Kushnir <kushnir.victor@gmail.com>
2021-03-03 22:37:47 -0500

    2.1B done coding, 2.1C WTF is this ?, from now we work with the .odt text file.

---
+++ Task 2.1C-TODO./sniffingPasswords.c
@@ -0,0 +1,11 @@
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
+#include <string.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
